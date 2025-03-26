60b7e784fd1e2d960dc31e72e520e780adfae873
mnblski <mnblski@gmail.com>
2025-03-26 23:21:52 +0000

    feat: add useDistanceScale hook to handle scaling the object based on the mouse position

---
+++ src/hooks/useDistanceScale.ts
@@ -0,0 +1,39 @@
+import { useTransform, useMotionValue } from "framer-motion";
+import { useMousePosition } from "./useMousePosition";
+import { useState } from "react";
+
+export const useDistanceScale = (minScale = 0.5, maxScale = 1.5) => {
+  const mousePosition = useMousePosition();
+  const [hasMouseMoved, setHasMouseMoved] = useState(false);
+
+  const x = useMotionValue(mousePosition.x);
+  const y = useMotionValue(mousePosition.y);
+
+  x.set(mousePosition.x);
+  y.set(mousePosition.y);
+
+  // Set hasMouseMoved to true when mouse position changes from initial values
+  if (mousePosition.x !== 0 || mousePosition.y !== 0) {
+    !hasMouseMoved && setHasMouseMoved(true);
+  }
+
+  const scale = useTransform([x, y], (latest: number[]) => {
+    if (!hasMouseMoved) {
+      return 1.0; // Return normal scale until mouse moves
+    }
+
+    const distanceFromCenter = Math.sqrt(
+      Math.pow(latest[0] - window.innerWidth / 2, 2) +
+        Math.pow(latest[1] - window.innerHeight / 2, 2)
+    );
+
+    const maxDistance = Math.sqrt(
+      Math.pow(window.innerWidth / 2, 2) + Math.pow(window.innerHeight / 2, 2)
+    );
+
+    const normalizedDistance = 1 - distanceFromCenter / maxDistance;
+    return minScale + normalizedDistance * (maxScale - minScale);
+  });
+
+  return scale;
+};
