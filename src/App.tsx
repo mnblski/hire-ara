ae16833a2ded102599f3e93100c4f975d7795474
mnblski <mnblski@gmail.com>
2025-03-26 22:31:48 +0000

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <img src={reactLogo} className="logo react" alt="React logo" />
+    </>
+  );
+}
+
+export default App;
